02a2104f8eb05e1851ba470f291c653701040224
HimanshuHeda <harsh967896@gmail.com>
2024-12-11 00:12:23 +0530

    volume

---
+++ vol.java
@@ -0,0 +1,28 @@
+class Rect {
+    int length, breadth;
+
+    Rect(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+}
+
+class Cuboid extends Rect {
+    double height;
+
+    Cuboid(int l, int b, int h) {
+        super(l, b);
+        height = h;
+    }
+
+    void volume() {
+        System.out.println(length * breadth * height);
+    }
+}
+
+public class vol {
+    public static void main(String args[]) {
+        Cuboid v = new Cuboid(1, 2, 3);
+        v.volume();
+    }
+}
