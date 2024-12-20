1da029cfd832296f2bd993b316a56e64f74c4324
HimanshuHeda <harsh967896@gmail.com>
2024-12-20 11:23:18 +0530

    constructor calling

--- CallingCons.java
+++ CallingCons.java
@@ -1,28 +1,23 @@
-class A
-{
-    A()
-    {
+class A {
+    A() {
         System.out.println("Inside A's constructor.");
     }
 }
-class B extends A
-{
-    B()
-    {
+
+class B extends A {
+    B() {
         System.out.println("Inside B's constructor.");
     }
 }
-class C extends B
-{
-    C()
-    {
+
+class C extends B {
+    C() {
         System.out.println("Inside C's constructor.");
     }
 }
-class CallingCons
-{
-    public static void main(String args[])
-    {
+
+class CallingCons {
+    public static void main(String args[]) {
         C c = new C();
     }
 }
