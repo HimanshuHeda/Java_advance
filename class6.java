a4b49e7327e358f27dea9e0340f6ea77d27e7211
HimanshuHeda <harsh967896@gmail.com>
2024-12-20 11:26:09 +0530

    constructor overloading

--- class6.java
+++ class6.java
@@ -1,29 +1,42 @@
-class box{
-    int h,b,w;
-    box(int h, int b, int w){
+class Box {
+    int h, b, w;
+
+    Box(int h, int b, int w) {
         this.h = h;
+        this.b = b;
+        this.w = w;
+    }
+
+    Box(int h, int b) {
+        this.h = h;
+        this.b = b;
+        this.w = 1; // Default value for width
+    }
+
+    Box(int h) {
+        this.h = h;
+        this.b = 1; // Default value for breadth
+        this.w = 1; // Default value for width
+    }
+
+    int cal_vol() {
+        return h * b * w;
     }
 }
 
-class box{
-    int cal_vol(){
-        return h*b*w;
-    }
-}
 public class class6 {
     public static void main(String[] args) {
-        box b1 = new box(10, 20, 30);
-        box b2 = new box(5,7);
-        box b3 = new box(10);
+        Box b1 = new Box(10, 20, 30);
+        Box b2 = new Box(5, 7);
+        Box b3 = new Box(10);
 
-        int v=b1.cal_vol();
-        System.out.println("The volume of box b1 is : " +v);
+        int v1 = b1.cal_vol();
+        System.out.println("The volume of box b1 is : " + v1);
 
-        int v=b2.cal_vol();
-        System.out.println("The volume of box b2 is : " +v);
+        int v2 = b2.cal_vol();
+        System.out.println("The volume of box b2 is : " + v2);
 
-        int v=b3.cal_vol();
-        System.out.println("The volume of box b3 is : " +v);
-        
+        int v3 = b3.cal_vol();
+        System.out.println("The volume of box b3 is : " + v3);
     }
 }
