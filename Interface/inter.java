fb2a9d3111496a79ad0867aada94312ff1453dd1
HimanshuHeda <harsh967896@gmail.com>
2025-01-04 12:43:09 +0530

    interface

---
+++ Interface/inter.java
@@ -0,0 +1,33 @@
+package Interface;
+
+interface Shape{
+    int calculateArea();
+    int calculatePerimeter();
+}
+
+class Rectangle implements Shape {
+    int length;
+    int width;
+
+    public Rectangle(int length,int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public int calculateArea(){
+        return length * width;
+    }
+
+    @Override
+    public int calculatePerimeter(){
+        return 2 * (length + width);
+    }
+}
+public class inter {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 6);
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Perimeter of Rectangle: " + rectangle.calculatePerimeter());
+    }
+}
